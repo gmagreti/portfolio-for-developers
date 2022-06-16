8f7bac412a2067f89e53c1bbed26c18d4649fd33
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-06-16 19:08:36 -0300

    style(Theme): adding to colors  in theme.

---
+++ src/styles/theme.ts
@@ -0,0 +1,61 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem'
+    }
+  },
+  colors: {
+    primary_100: '#01080E',
+    primary_200: '#011627',
+    primary_300: '#011627',
+    secondary_100: '#607B96',
+    secondary_200: '#3C9D93',
+    secondary_300: '#4D5BCE',
+    secondary_400: '#FFFFFF',
+    accent_100: '#FEA55F',
+    accent_200: '#43D9AD',
+    accent_300: '#E99287',
+    accent_400: '#C98BDF',
+    lines: '#1E2D3D',
+    gradients_100: '#4D5BCE',
+    gradients_200: '#43D9AD'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+} as const
